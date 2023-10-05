76bab6c38c2a95a7b79bca287dd2ae1c124d0b8d
LeGiaLuat <luatb2007195@student.ctu.edu.vn>
2023-10-05 15:38:05 +0700

    Cap nhat

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const app = require("./app");
-const config = require("./app/config")
-const MongoDB = require("./app/utils/mongodb.util");
+const config = require("./app/config");
+const MongoDb = require("./app/utils/mongodb.util");
 
 async function startServer() {
-  try{
-    // Connect to MongoDB
-    await MongoDB.connect(config.db.uri);
-    console.log("Connected to MongoDB successfully");
+    try {
+        await MongoDb.connect(config.db.uri);
+        console.log("Connected to the database!");
 
-    const PORT = config.app.port;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch(error){
-      console.log("Cannot connect to the database",error);
-      process.exit(0);
-  }  
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!" ,error);
+        process.exit();
+    }
 }
 
 startServer();
