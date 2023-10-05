e0495b66a51991f0556eb7eb84c17bfff8fc3187
LeGiaLuat <luatb2007195@student.ctu.edu.vn>
2023-10-05 13:54:22 +0700

    Cai dat express chay thong bao xin chao

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,7 @@
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+
 exports.create = (req, res) => {
     res.send({ message: "create handler"});
 };
@@ -24,4 +28,21 @@ exports.deleteAll = (req, res) => {
 
 exports.findAllFavorite = (req, res) => {
     res.send({ message: "findAllFavorite handler"});
+};
+
+// Create and save a new contact
+exports.create = async (req, res, next) => {
+    if(!req.body?.name) {
+        return next(new ApiError(400, "Name is required"));
+    }
+
+    try{
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error){   
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
